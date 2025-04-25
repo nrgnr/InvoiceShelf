ce00ac6c06bf268dd5c8cfa245756b5ba130288d
nrgnr <your.email@example.com>
2025-04-24 21:59:26 -0500

    Downgrade Vite and Laravel Vite Plugin dependencies in package.json and clean up Vite configuration
    - Downgraded @vitejs/plugin-vue to version 4.5.0, laravel-vite-plugin to version 0.8.1, and vite to version 5.1.4 for compatibility.
    - Removed unnecessary optimization settings and comments from vite.config.js to simplify the configuration.

--- vite.config.js
+++ vite.config.js
@@ -13,35 +13,15 @@ export default defineConfig({
         },
         extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.mjs']
     },
-    optimizeDeps: {
-        exclude: ['@tiptap/pm']
-    },
     build: {
         chunkSizeWarningLimit: 1000,
         sourcemap: false,
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true
-            }
-        }
     },
     plugins: [
         vue({
             template: {
                 transformAssetUrls: {
-                    // The Vue plugin will re-write asset URLs, when referenced
-                    // in Single File Components, to point to the Laravel web
-                    // server. Setting this to `null` allows the Laravel plugin
-                    // to instead re-write asset URLs to point to the Vite
-                    // server instead.
                     base: null,
-
-                    // The Vue plugin will parse absolute URLs and treat them
-                    // as absolute paths to files on disk. Setting this to
-                    // `false` will leave absolute URLs un-touched so they can
-                    // reference assets in the public directory as expected.
                     includeAbsolute: false,
                 },
             },
